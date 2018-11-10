36bd7342834fe131d001607eeb08399c953de756
Jared <jascnu@gmail.com>
2018-11-10 21:09:42 +0000

    Javascript

---
+++ mongo-cards/public/javascripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $("#postComment").click(function() {
+    var myobj = { Name: $("#name").val(), Comment: $("#comment").val() };
+    jobj = JSON.stringify(myobj);
+    $("#json").html(jobj);
+    var url = "comment";
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: jobj,
+      contentType: "application/json; charset=utf-8",
+      success: function(data, textStatus) {
+        $("#done").html(textStatus);
+      }
+    })
+  });
+  
+  $("#deleteComments").click(function() {
+    $.ajax({
+      url: 'comment',
+      type: "DELETE",
+      success: function(data, textStatus) {
+        $("#done").html(textStatus);
+      }
+    })
+    //get rid of everything in the comments div too
+    $("#comments").html("");
+  });
+
+$("#getComments").click(function() {
+  var url = 'comment?q=' + $('#query').val();
+    $.getJSON(url, function(data) {
+      console.log(data);
+      //var everything = "<ul>";
+      var everything ="";
+      for (var comment in data) {
+        com = data[comment];
+        //everything += "<li> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+        everything += "<div class=\"card bg-dark text-white\"> <div class=\"card-body\"> Name: " + com.Name + " -- Comment: " + com.Comment + "</div> </div>";
+      }
+      everything += "<br>";
+      $("#comments").html(everything);
+    })
+  });
+});
